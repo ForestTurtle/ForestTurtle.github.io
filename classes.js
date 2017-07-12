f5be39e7e6e8e0e2e806607d5364acd5fced2346
Jefferson Wang <Jefferson Wang>
2017-07-12 15:18:25 -0400

    sepearted classes to different file, crerated game.js

---
+++ classes.js
@@ -0,0 +1,22 @@
+class Card {
+	constructor(color, number){
+		this.color = color;
+		this.number = number;
+	}
+}
+
+class HitArea {
+	constructor(x, y, w, h, action){
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.action = action;
+	}
+}
+
+class DynamicDrawable {
+	constructor(draw){
+		this.draw = draw;
+	}
+}
